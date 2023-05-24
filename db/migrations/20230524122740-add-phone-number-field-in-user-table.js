a4f8c628768f4fc84707bae6cea514eafb57e676
Farhan Ramadhan <nak.rokesselalu@gmail.com>
2023-05-24 20:07:55 +0700

    user already have phone number

---
+++ db/migrations/20230524122740-add-phone-number-field-in-user-table.js
@@ -0,0 +1,24 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Users', 'phoneNumber', {
+      type: Sequelize.STRING(15),
+    });
+  },
+
+  async down(queryInterface) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('Users', 'phoneNumber');
+  },
+};
--- db/models/user.js
+++ db/models/user.js
@@ -20,6 +20,7 @@ module.exports = (sequelize, DataTypes) => {
       name: DataTypes.STRING,
       email: DataTypes.STRING,
       password: DataTypes.STRING,
+      phoneNumber: DataTypes.STRING(15),
     },
     {
       sequelize,
